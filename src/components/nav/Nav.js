da70f3a45e363dc14d8a975670b705186e44ba11
Riya Agarwal <53482709+riyagarwal@users.noreply.github.com>
2023-04-21 11:38:15 +0530

    manage expense page done

--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,8 +1,31 @@
+import { useNavigate } from "react-router-dom";
+import { Card, CardBody, Button, Text, Flex } from "@chakra-ui/react";
+import { PATHS } from "../../paths";
 
 const Nav = () => {
+  const navigate = useNavigate();
   return (
-    <div>Nav</div>
-  )
-}
+    <div>
+      <Card mb={50}>
+        <CardBody>
+          <Flex justifyContent="space-between" alignItems="center">
+            <Text color="dodgerblue" fontWeight="800" fontSize="2em">
+              Money Movement Tracker
+            </Text>
+            <Button
+              colorScheme="red"
+              onClick={() => {
+                window.localStorage.clear();
+                navigate(PATHS.LOGIN);
+              }}
+            >
+              Logout
+            </Button>
+          </Flex>
+        </CardBody>
+      </Card>
+    </div>
+  );
+};
 
-export default Nav
+export default Nav;
